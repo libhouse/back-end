29e9bd24405e269fc4ca1609741faf0074eb79f8
Lucas Dirani <lruizd@protonmail.com>
2022-11-17 15:38:26 -0300

    Criação da classe ChargePreferences junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/Charges/ChargePreferences.cs
@@ -0,0 +1,28 @@
+﻿namespace LibHouse.Business.Entities.Residents.Preferences.Charges
+{
+    public class ChargePreferences
+    {
+        public RentPreferences RentPreferences { get; private set; }
+        public ExpensePreferences ExpensePreferences { get; private set; }
+
+        public void AddRentPreferences(decimal minimumRentalAmountDesired, decimal maximumRentalAmountDesired)
+        {
+            RentPreferences = new(minimumRentalAmountDesired, maximumRentalAmountDesired);
+        }
+
+        public bool HaveRentPreferences()
+        {
+            return RentPreferences is not null;
+        }
+
+        public void AddExpensePreferences(decimal minimumExpenseAmountDesired, decimal maximumExpenseAmountDesired)
+        {
+            ExpensePreferences = new(minimumExpenseAmountDesired, maximumExpenseAmountDesired);
+        }
+
+        public bool HaveExpensePreferences()
+        {
+            return ExpensePreferences is not null;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/Charges/ChargePreferencesTests.cs
@@ -0,0 +1,25 @@
+﻿using LibHouse.Business.Entities.Residents.Preferences.Charges;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.Charges
+{
+    [Collection("Business.Entities")]
+    public class ChargePreferencesTests
+    {
+        [Fact]
+        public void AddRentPreferences_ValidRentPreferences_ShouldAddRentPreferences()
+        {
+            ChargePreferences chargePreferences = new();
+            chargePreferences.AddRentPreferences(minimumRentalAmountDesired: 150.0m, maximumRentalAmountDesired: 350.0m);
+            Assert.True(chargePreferences.HaveRentPreferences());
+        }
+
+        [Fact]
+        public void AddExpensePreferences_ValidExpensePreferences_ShouldAddExpensePreferences()
+        {
+            ChargePreferences chargePreferences = new();
+            chargePreferences.AddExpensePreferences(minimumExpenseAmountDesired: 150.0m, maximumExpenseAmountDesired: 350.0m);
+            Assert.True(chargePreferences.HaveExpensePreferences());
+        }
+    }
+}
