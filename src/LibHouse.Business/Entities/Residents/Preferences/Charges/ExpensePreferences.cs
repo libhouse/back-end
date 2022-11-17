cd243148fb5c1db963f1ee85e96f036a8897bbae
Lucas Dirani <lruizd@protonmail.com>
2022-11-17 15:29:52 -0300

    Criação da classe ExpensePreferences junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/Charges/ExpensePreferences.cs
@@ -0,0 +1,26 @@
+﻿using LibHouse.Business.Entities.Shared;
+
+namespace LibHouse.Business.Entities.Residents.Preferences.Charges
+{
+    public record ExpensePreferences
+    {
+        public MonetaryRange ExpenseRange { get; init; }
+
+        public ExpensePreferences(
+            decimal minimumExpenseAmountDesired,
+            decimal maximumExpenseAmountDesired)
+        {
+            ExpenseRange = new(minimumExpenseAmountDesired, maximumExpenseAmountDesired);
+        }
+
+        public decimal GetMinimumExpenseAmount()
+        {
+            return ExpenseRange.MinimumValue;
+        }
+
+        public decimal GetMaximumExpenseAmount()
+        {
+            return ExpenseRange.MaximumValue;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/Charges/ExpensePreferencesTests.cs
@@ -0,0 +1,28 @@
+﻿using LibHouse.Business.Entities.Residents.Preferences.Charges;
+using System;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.Charges
+{
+    [Collection("Business.Entities")]
+    public class ExpensePreferencesTests
+    {
+        [Fact]
+        public void ExpensePreferences_ValidExpensePreferences_ShouldCreateExpensePreferences()
+        {
+            decimal minimumExpenseAmountDesired = 100.0m;
+            decimal maximumExpenseAmountDesired = 200.0m;
+            ExpensePreferences expensePreferences = new(minimumExpenseAmountDesired, maximumExpenseAmountDesired);
+            Assert.Equal(minimumExpenseAmountDesired, expensePreferences.GetMinimumExpenseAmount());
+            Assert.Equal(maximumExpenseAmountDesired, expensePreferences.GetMaximumExpenseAmount());
+        }
+
+        [Fact]
+        public void ExpensePreferences_InvalidExpensePreferences_ShouldNotCreateExpensePreferences()
+        {
+            decimal minimumExpenseAmountDesired = 100.0m;
+            decimal maximumExpenseAmountDesired = 50.0m;
+            Assert.ThrowsAny<ArgumentOutOfRangeException>(() => new ExpensePreferences(minimumExpenseAmountDesired, maximumExpenseAmountDesired));
+        }
+    }
+}
