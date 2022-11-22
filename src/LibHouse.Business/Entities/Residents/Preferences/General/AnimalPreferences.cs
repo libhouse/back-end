63f05aa4bcd18a8ddd8bfa3380aeba76d2580463
Lucas Dirani <lruizd@protonmail.com>
2022-11-22 17:33:10 -0300

    Criação da classe AnimalPreferences junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/General/AnimalPreferences.cs
@@ -0,0 +1,17 @@
+﻿namespace LibHouse.Business.Entities.Residents.Preferences.General
+{
+    public record AnimalPreferences
+    {
+        public AnimalPreferences(bool wantSpaceForAnimals)
+        {
+            WantSpaceForAnimals = wantSpaceForAnimals;
+        }
+
+        public bool WantSpaceForAnimals { get; }
+
+        public bool RequiresAcceptanceOfAnimals()
+        {
+            return WantSpaceForAnimals;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/General/AnimalPreferencesTests.cs
@@ -0,0 +1,18 @@
+﻿using LibHouse.Business.Entities.Residents.Preferences.General;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.General
+{
+    [Collection("Business.Entities")]
+    public class AnimalPreferencesTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void AnimalPreferences_ValidAnimalPreferences_ShouldCreateAnimalPreferences(bool wantSpaceForAnimals)
+        {
+            AnimalPreferences animalPreferences = new(wantSpaceForAnimals);
+            Assert.Equal(wantSpaceForAnimals, animalPreferences.RequiresAcceptanceOfAnimals());
+        }
+    }
+}
