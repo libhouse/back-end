94f12a2de23b10c85bebaa0a8f232ed2f1c004c4
Lucas Dirani <lruizd@protonmail.com>
2022-11-22 17:21:51 -0300

    Criação da classe SmokersPreferences junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/General/SmokersPreferences.cs
@@ -0,0 +1,17 @@
+﻿namespace LibHouse.Business.Entities.Residents.Preferences.General
+{
+    public record SmokersPreferences
+    {
+        public SmokersPreferences(bool acceptSmokers)
+        {
+            AcceptSmokers = acceptSmokers;
+        }
+
+        public bool AcceptSmokers { get; }
+
+        public bool RequiresAcceptanceOfSmokers()
+        {
+            return AcceptSmokers;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/General/SmokersPreferencesTests.cs
@@ -0,0 +1,18 @@
+﻿using LibHouse.Business.Entities.Residents.Preferences.General;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.General
+{
+    [Collection("Business.Entities")]
+    public class SmokersPreferencesTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void SmokersPreferences_ValidSmokersPreferences_ShouldCreateSmokersPreferences(bool acceptSmokers)
+        {
+            SmokersPreferences smokersPreferences = new(acceptSmokers);
+            Assert.Equal(acceptSmokers, smokersPreferences.RequiresAcceptanceOfSmokers());
+        }
+    }
+}
