08ec60e15ee7d0c813f1c02f1d8d3d1bf89ecc07
Lucas Dirani <lruizd@protonmail.com>
2022-11-22 17:27:52 -0300

    Criação da classe PartyPreferences junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/General/PartyPreferences.cs
@@ -0,0 +1,17 @@
+﻿namespace LibHouse.Business.Entities.Residents.Preferences.General
+{
+    public record PartyPreferences
+    {
+        public PartyPreferences(bool wantsToParty)
+        {
+            WantsToParty = wantsToParty;
+        }
+
+        public bool WantsToParty { get; }
+
+        public bool RequiresAcceptanceOfParties()
+        {
+            return WantsToParty;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/General/PartyPreferencesTests.cs
@@ -0,0 +1,18 @@
+﻿using LibHouse.Business.Entities.Residents.Preferences.General;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.General
+{
+    [Collection("Business.Entities")]
+    public record PartyPreferencesTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void PartyPreferences_ValidPartyPreferences_ShouldCreatePartyPreferences(bool wantsToParty)
+        {
+            PartyPreferences partyPreferences = new(wantsToParty);
+            Assert.Equal(wantsToParty, partyPreferences.RequiresAcceptanceOfParties());
+        }
+    }
+}
