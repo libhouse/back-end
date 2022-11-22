cb7ee6b9ea56eb7b076d0b7f54fef58d8cd22e76
Lucas Dirani <lruizd@protonmail.com>
2022-11-22 17:16:48 -0300

    Criação da classe ChildrenPreferences junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/General/ChildrenPreferences.cs
@@ -0,0 +1,17 @@
+﻿namespace LibHouse.Business.Entities.Residents.Preferences.General
+{
+    public record ChildrenPreferences
+    {
+        public ChildrenPreferences(bool acceptChildren)
+        {
+            AcceptChildren = acceptChildren;
+        }
+
+        public bool AcceptChildren { get; }
+
+        public bool RequiresAcceptanceOfChildren()
+        {
+            return AcceptChildren;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/General/ChildrenPreferencesTests.cs
@@ -0,0 +1,18 @@
+﻿using LibHouse.Business.Entities.Residents.Preferences.General;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.General
+{
+    [Collection("Business.Entities")]
+    public class ChildrenPreferencesTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ChildrenPreferences_ValidChildrenPreferences_ShouldCreateChildrenPreferences(bool acceptChildren)
+        {
+            ChildrenPreferences childrenPreferences = new(acceptChildren);
+            Assert.Equal(acceptChildren, childrenPreferences.RequiresAcceptanceOfChildren());
+        }
+    }
+}
