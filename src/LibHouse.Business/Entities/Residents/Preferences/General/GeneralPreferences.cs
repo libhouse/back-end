738f6154131d74bce0526c778c68329cb2b55f5b
Lucas Dirani <lruizd@protonmail.com>
2022-11-23 14:34:46 -0300

    Criação da classe GeneralPreferences junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/General/GeneralPreferences.cs
@@ -0,0 +1,69 @@
+﻿using LibHouse.Business.Entities.Users;
+
+namespace LibHouse.Business.Entities.Residents.Preferences.General
+{
+    public class GeneralPreferences
+    {
+        public AnimalPreferences AnimalPreferences { get; private set; }
+        public ChildrenPreferences ChildrenPreferences { get; private set; }
+        public PartyPreferences PartyPreferences { get; private set; }
+        public RoommatePreferences RoommatePreferences { get; private set; }
+        public SmokersPreferences SmokersPreferences { get; private set; }
+
+        public void AddAnimalPreferences(bool wantSpaceForAnimals)
+        {
+            AnimalPreferences = new(wantSpaceForAnimals);
+        }
+
+        public bool HaveAnimalPreferences()
+        {
+            return AnimalPreferences is not null;
+        }
+
+        public void AddChildrenPreferences(bool acceptChildren)
+        {
+            ChildrenPreferences = new(acceptChildren);
+        }
+
+        public bool HaveChildrenPreferences()
+        {
+            return ChildrenPreferences is not null;
+        }
+
+        public void AddPartyPreferences(bool wantsToParty)
+        {
+            PartyPreferences = new(wantsToParty);
+        }
+
+        public bool HavePartyPreferences()
+        {
+            return PartyPreferences is not null;
+        }
+
+        public void AddRoommatePreferences(
+            int minimumNumberOfRoommatesDesired,
+            int maximumNumberOfRoommatesDesired,
+            Gender[] acceptedGendersOfRoommates)
+        {
+            RoommatePreferences = new(
+                minimumNumberOfRoommatesDesired,
+                maximumNumberOfRoommatesDesired, 
+                acceptedGendersOfRoommates);
+        }
+
+        public bool HaveRoommatePreferences()
+        {
+            return RoommatePreferences is not null;
+        }
+
+        public void AddSmokersPreferences(bool acceptSmokers)
+        {
+            SmokersPreferences = new(acceptSmokers);
+        }
+
+        public bool HaveSmokersPreferences()
+        {
+            return SmokersPreferences is not null;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/General/GeneralPreferencesTests.cs
@@ -0,0 +1,53 @@
+﻿using LibHouse.Business.Entities.Residents.Preferences.General;
+using LibHouse.Business.Entities.Users;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.General
+{
+    [Collection("Business.Entities")]
+    public class GeneralPreferencesTests
+    {
+        [Fact]
+        public void AddAnimalPreferences_ValidAnimalPreferences_ShouldAddAnimalPreferences()
+        {
+            GeneralPreferences generalPreferences = new();
+            generalPreferences.AddAnimalPreferences(wantSpaceForAnimals: true);
+            Assert.True(generalPreferences.HaveAnimalPreferences());
+        }
+
+        [Fact]
+        public void AddChildrenPreferences_ValidChildrenPreferences_ShouldAddChildrenPreferences()
+        {
+            GeneralPreferences generalPreferences = new();
+            generalPreferences.AddChildrenPreferences(acceptChildren: true);
+            Assert.True(generalPreferences.HaveChildrenPreferences());
+        }
+
+        [Fact]
+        public void AddPartyPreferences_ValidPartyPreferences_ShouldAddPartyPreferences()
+        {
+            GeneralPreferences generalPreferences = new();
+            generalPreferences.AddPartyPreferences(wantsToParty: true);
+            Assert.True(generalPreferences.HavePartyPreferences());
+        }
+
+        [Fact]
+        public void AddRoommatePreferences_ValidRoommatePreferences_ShouldAddRoommatePreferences()
+        {
+            GeneralPreferences generalPreferences = new();
+            int minimumNumberOfRoommatesDesired = 1;
+            int maximumNumberOfRoommatesDesired = 5;
+            Gender[] acceptedGendersOfRoommates = new[] { Gender.Male };
+            generalPreferences.AddRoommatePreferences(minimumNumberOfRoommatesDesired, maximumNumberOfRoommatesDesired, acceptedGendersOfRoommates);
+            Assert.True(generalPreferences.HaveRoommatePreferences());
+        }
+
+        [Fact]
+        public void AddSmokersPreferences_ValidSmokersPreferences_ShouldAddSmokersPreferences()
+        {
+            GeneralPreferences generalPreferences = new();
+            generalPreferences.AddSmokersPreferences(acceptSmokers: true);
+            Assert.True(generalPreferences.HaveSmokersPreferences());
+        }
+    }
+}
