4be13b5081b1853df637631ce10b50801439e098
Lucas Dirani <lruizd@protonmail.com>
2022-11-24 14:48:57 -0300

    Criação da classe Street junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Localizations/Street.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LibHouse.Business.Entities.Localizations
+{
+    public record Street
+    {
+        private const int MAXIMUM_LENGTH_ALLOWED_NAME = 60;
+
+        public Street(
+            string name, 
+            string streetNeighborhood, 
+            string streetCity,
+            string abbreviationOfCityFederativeUnit)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name), "O nome do logradouro é obrigatório");
+            }
+            if (name.Length > MAXIMUM_LENGTH_ALLOWED_NAME)
+            {
+                throw new ArgumentException(paramName: nameof(name), message: $"O comprimento do nome não pode ultrapassar {MAXIMUM_LENGTH_ALLOWED_NAME} caracteres");
+            }
+            if (!Regex.IsMatch(name, @"^[\p{L}\p{M}\s]+$", RegexOptions.Compiled))
+            {
+                throw new FormatException("O nome do logradouro deve possuir apenas letras");
+            }
+            Name = name;
+            Neighborhood = new(streetNeighborhood, streetCity, abbreviationOfCityFederativeUnit);
+        }
+
+        private string Name { get; init; }
+        private Neighborhood Neighborhood { get; init; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetNeighborhoodName()
+        {
+            return Neighborhood.GetName();
+        }
+
+        public string GetAbbreviationOfTheFederativeUnit()
+        {
+            return Neighborhood.GetAbbreviationOfTheFederativeUnit();
+        }
+
+        public string GetCityName()
+        {
+            return Neighborhood.GetCityName();
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Localizations/StreetTests.cs
@@ -0,0 +1,34 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using System;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Localizations
+{
+    [Collection("Business.Entities")]
+    public class StreetTests
+    {
+        [Theory]
+        [InlineData("Rua Hidrolândia", "Tucuruvi", "São Paulo", "SP")]
+        [InlineData("Rua Ipanema", "Ipanema", "Rio de Janeiro", "RJ")]
+        public void Street_ValidStreet_ShouldCreateStreet(string name, string streetNeighborhood, string streetCity, string abbreviationOfCityFederativeUnit)
+        {
+            Street street = new(name, streetNeighborhood, streetCity, abbreviationOfCityFederativeUnit);
+            Assert.Equal(name, street.GetName());
+            Assert.Equal(streetNeighborhood, street.GetNeighborhoodName());
+            Assert.Equal(streetCity, street.GetCityName());
+            Assert.Equal(abbreviationOfCityFederativeUnit, street.GetAbbreviationOfTheFederativeUnit());
+        }
+
+        [Theory]
+        [InlineData("", "Tucuruvi", "São Paulo", "SP")]
+        [InlineData("Nome para o logradouro com um comprimento maior do que o permitido", "Ipanema", "Rio de Janeiro", "RJ")]
+        [InlineData("Logradouro 1000", "Tucuruvi", "São Paulo", "SP")]
+        [InlineData("Rua Hidrolândia", "", "São Paulo", "SP")]
+        [InlineData("Rua Hidrolândia", "Tucuruvi", "", "SP")]
+        [InlineData("Rua Hidrolândia", "Tucuruvi", "São Paulo", "")]
+        public void Street_InvalidStreet_ShouldNotCreateStreet(string name, string streetNeighborhood, string streetCity, string abbreviationOfCityFederativeUnit)
+        {
+            Assert.ThrowsAny<Exception>(() => new Street(name, streetNeighborhood, streetCity, abbreviationOfCityFederativeUnit));
+        }
+    }
+}
