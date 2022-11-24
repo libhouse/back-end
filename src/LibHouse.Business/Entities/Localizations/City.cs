2fe3bb96ab1883c3d5274870dc3eb18825d1a965
Lucas Dirani <lruizd@protonmail.com>
2022-11-24 14:14:59 -0300

    Criação da classe City junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Localizations/City.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LibHouse.Business.Entities.Localizations
+{
+    public record City
+    {
+        private const int MAXIMUM_LENGTH_ALLOWED_NAME = 30;
+
+        public City(string name, string abbreviationOfCityFederativeUnit)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name), "O nome da cidade é obrigatório");
+            }
+            if (name.Length > MAXIMUM_LENGTH_ALLOWED_NAME)
+            {
+                throw new ArgumentException(paramName: nameof(name), message: $"O comprimento do nome não pode ultrapassar {MAXIMUM_LENGTH_ALLOWED_NAME} caracteres");
+            }
+            if (!Regex.IsMatch(name, @"^[\p{L}\p{M}\s]+$", RegexOptions.Compiled))
+            {
+                throw new FormatException("O nome da cidade deve possuir apenas letras");
+            }
+            Name = name;
+            FederativeUnit = new(abbreviationOfCityFederativeUnit);
+        }
+
+        private string Name { get; init; }
+        private FederativeUnit FederativeUnit { get; init; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetAbbreviationOfTheFederativeUnit()
+        {
+            return FederativeUnit.GetAbbreviation();
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Localizations/CityTests.cs
@@ -0,0 +1,30 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using System;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Localizations
+{
+    [Collection("Business.Entities")]
+    public class CityTests
+    {
+        [Theory]
+        [InlineData("São Paulo", "SP")]
+        [InlineData("Rio de Janeiro", "RJ")]
+        public void City_ValidCity_ShouldCreateCity(string name, string abbreviationOfCityFederativeUnit)
+        {
+            City city = new(name, abbreviationOfCityFederativeUnit);
+            Assert.Equal(name, city.GetName());
+            Assert.Equal(abbreviationOfCityFederativeUnit, city.GetAbbreviationOfTheFederativeUnit());
+        }
+
+        [Theory]
+        [InlineData("Cidade 1000", "SP")]
+        [InlineData("", "RJ")]
+        [InlineData("Nome de cidade com tamanho maior do que o permitido", "RJ")]
+        [InlineData("São Paulo", "")]
+        public void City_InvalidCity_ShouldNotCreateCity(string name, string abbreviationOfCityFederativeUnit)
+        {
+            Assert.ThrowsAny<Exception>(() => new City(name, abbreviationOfCityFederativeUnit));
+        }
+    }
+}
