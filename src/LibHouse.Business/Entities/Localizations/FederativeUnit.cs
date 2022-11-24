a1b5343654702e9ab4b527ae1d1a2af18b13e735
Lucas Dirani <lruizd@protonmail.com>
2022-11-24 13:48:08 -0300

    Criação da classe FederativeUnit junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Localizations/FederativeUnit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace LibHouse.Business.Entities.Localizations
+{
+    public record FederativeUnit
+    {
+        private const int ABBREVIATION_LENGTH_FEDERATIVE_UNIT = 2;
+
+        public FederativeUnit(string abbreviationOfTheFederativeUnit)
+        {
+            if (string.IsNullOrEmpty(abbreviationOfTheFederativeUnit))
+            {
+                throw new ArgumentNullException(nameof(abbreviationOfTheFederativeUnit), "A sigla da unidade federativa é obrigatória");
+            }
+            if (abbreviationOfTheFederativeUnit.Length != ABBREVIATION_LENGTH_FEDERATIVE_UNIT)
+            {
+                throw new ArgumentException(paramName: nameof(abbreviationOfTheFederativeUnit), message: $"O comprimento da sigla deve ser igual a {ABBREVIATION_LENGTH_FEDERATIVE_UNIT}");
+            }
+            if (!abbreviationOfTheFederativeUnit.All(char.IsLetter))
+            {
+                throw new FormatException("A sigla da unidade federativa deve possuir apenas letras");
+            }
+            AbbreviationOfTheFederativeUnit = abbreviationOfTheFederativeUnit.ToUpper();
+        }
+
+        private string AbbreviationOfTheFederativeUnit { get; init; }
+
+        public string GetAbbreviation()
+        {
+            return AbbreviationOfTheFederativeUnit;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Localizations/FederativeUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using System;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Localizations
+{
+    [Collection("Business.Entities")]
+    public class FederativeUnitTests
+    {
+        [Theory]
+        [InlineData("SP")]
+        [InlineData("RJ")]
+        public void FederativeUnit_ValidFederativeUnit_ShouldCreateFederativeUnit(string abbreviationOfTheFederativeUnit)
+        {
+            FederativeUnit federativeUnit = new(abbreviationOfTheFederativeUnit);
+            Assert.Equal(abbreviationOfTheFederativeUnit, federativeUnit.GetAbbreviation());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("SSP")]
+        [InlineData("S")]
+        [InlineData("22")]
+        public void FederativeUnit_InvalidFederativeUnit_ShouldNotCreateFederativeUnit(string abbreviationOfTheFederativeUnit)
+        {
+            Assert.ThrowsAny<Exception>(() => new FederativeUnit(abbreviationOfTheFederativeUnit));
+        }
+    }
+}
