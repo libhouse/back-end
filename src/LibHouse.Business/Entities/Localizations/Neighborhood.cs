5a3c6027a9ddbe4b878789de55d5ff884f226dba
Lucas Dirani <lruizd@protonmail.com>
2022-11-24 14:33:33 -0300

    Criação da classe Neighborhood junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Localizations/Neighborhood.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LibHouse.Business.Entities.Localizations
+{
+    public record Neighborhood
+    {
+        private const int MAXIMUM_LENGTH_ALLOWED_NAME = 60;
+
+        public Neighborhood(string name, string neighborhoodCity, string abbreviationOfCityFederativeUnit)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name), "O nome do bairro é obrigatório");
+            }
+            if (name.Length > MAXIMUM_LENGTH_ALLOWED_NAME)
+            {
+                throw new ArgumentException(paramName: nameof(name), message: $"O comprimento do nome não pode ultrapassar {MAXIMUM_LENGTH_ALLOWED_NAME} caracteres");
+            }
+            if (!Regex.IsMatch(name, @"^[\p{L}\p{M}\s]+$", RegexOptions.Compiled))
+            {
+                throw new FormatException("O nome do bairro deve possuir apenas letras");
+            }
+            Name = name;
+            City = new(neighborhoodCity, abbreviationOfCityFederativeUnit);
+        }
+
+        private string Name { get; init; }
+        private City City { get; init; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetCityName()
+        {
+            return City.GetName();
+        }
+
+        public string GetAbbreviationOfTheFederativeUnit()
+        {
+            return City.GetAbbreviationOfTheFederativeUnit();
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Localizations/NeighborhoodTests.cs
@@ -0,0 +1,32 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using System;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Localizations
+{
+    [Collection("Business.Entities")]
+    public class NeighborhoodTests
+    {
+        [Theory]
+        [InlineData("Tucuruvi", "São Paulo", "SP")]
+        [InlineData("Ipanema", "Rio de Janeiro", "RJ")]
+        public void Neighborhood_ValidNeighborhood_ShouldCreateNeighborhood(string name, string neighborhoodCity, string abbreviationOfCityFederativeUnit)
+        {
+            Neighborhood neighborhood = new(name, neighborhoodCity, abbreviationOfCityFederativeUnit);
+            Assert.Equal(name, neighborhood.GetName());
+            Assert.Equal(neighborhoodCity, neighborhood.GetCityName());
+            Assert.Equal(abbreviationOfCityFederativeUnit, neighborhood.GetAbbreviationOfTheFederativeUnit());
+        }
+
+        [Theory]
+        [InlineData("", "São Paulo", "SP")]
+        [InlineData("Nome para o bairro com um comprimento maior do que o permitido", "Rio de Janeiro", "RJ")]
+        [InlineData("Bairro 1000", "São Paulo", "SP")]
+        [InlineData("Tucuruvi", "", "SP")]
+        [InlineData("Tucuruvi", "São Paulo", "")]
+        public void Neighborhood_InvalidNeighborhood_ShouldNotCreateNeighborhood(string name, string neighborhoodCity, string abbreviationOfCityFederativeUnit)
+        {
+            Assert.ThrowsAny<Exception>(() => new Neighborhood(name, neighborhoodCity, abbreviationOfCityFederativeUnit));
+        }
+    }
+}
