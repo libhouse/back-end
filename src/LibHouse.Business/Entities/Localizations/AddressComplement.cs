ae7f0739031ac42bcbef3cda3d2055fbe990d0ca
Lucas Dirani <lruizd@protonmail.com>
2022-11-25 13:28:12 -0300

    Criação da classe AddressComplement junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Localizations/AddressComplement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibHouse.Business.Entities.Localizations
+{
+    public record AddressComplement
+    {
+        public AddressComplement(string description)
+        {
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentNullException(nameof(description), "A descrição é obrigatória");
+            }
+            Description = description;
+        }
+
+        private string Description { get; init; }
+
+        public string GetDescription()
+        {
+            return Description;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Localizations/AddressComplementTests.cs
@@ -0,0 +1,18 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Localizations
+{
+    [Collection("Business.Entities")]
+    public class AddressComplementTests
+    {
+        [Theory]
+        [InlineData("Praça da Sé")]
+        [InlineData("Lote 04")]
+        public void AddressComplement_ValidAddressComplement_ShouldCreateAddressComplement(string description)
+        {
+            AddressComplement addressComplement = new(description);
+            Assert.Equal(description, addressComplement.GetDescription());
+        }
+    }
+}
