40379d2969701a92a2ab19a41768f02e63bbb6db
Lucas Dirani <lruizd@protonmail.com>
2022-11-25 13:44:36 -0300

    Criação da classe PostalCode junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Localizations/PostalCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LibHouse.Business.Entities.Localizations
+{
+    public record PostalCode
+    {
+        public PostalCode(string postalCodeNumber)
+        {
+            if (string.IsNullOrEmpty(postalCodeNumber))
+            {
+                throw new ArgumentNullException(nameof(postalCodeNumber), "O número é obrigatório");
+            }
+            if (!Regex.IsMatch(postalCodeNumber, @"^[0-9]{5}[-]?[0-9]{3}$", RegexOptions.Compiled))
+            {
+                throw new FormatException("O número deve possuir oito dígitos, com os três últimos separados opcionalmente por traço");
+            }
+            PostalCodeNumber = postalCodeNumber.Replace('-', ' ');
+        }
+
+        private string PostalCodeNumber { get; init; }
+
+        public string GetNumber()
+        {
+            return PostalCodeNumber;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Localizations/PostalCodeTests.cs
@@ -0,0 +1,30 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using System;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Localizations
+{
+    [Collection("Business.Entities")]
+    public class PostalCodeTests
+    {
+        [Theory]
+        [InlineData("01001-000")]
+        [InlineData("02307210")]
+        public void PostalCode_ValidPostalCode_ShouldCreatePostalCode(string postalCodeNumber)
+        {
+            PostalCode postalCode = new(postalCodeNumber);
+            Assert.Equal(postalCodeNumber.Replace('-', ' '), postalCode.GetNumber());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("0230721")]
+        [InlineData("023072101")]
+        [InlineData("01001-0000")]
+        [InlineData("010010-00")]
+        public void PostalCode_InvalidPostalCode_ShouldNotCreatePostalCode(string postalCodeNumber)
+        {
+            Assert.ThrowsAny<Exception>(() => new PostalCode(postalCodeNumber));
+        }
+    }
+}
