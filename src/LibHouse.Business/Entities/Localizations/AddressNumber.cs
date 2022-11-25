4563872fddbbec348ef962e89b33c8a27c5d1348
Lucas Dirani <lruizd@protonmail.com>
2022-11-25 13:57:31 -0300

    Criação da classe AddressNumber junto de seus testes unitários.

---
+++ src/LibHouse.Business/Entities/Localizations/AddressNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibHouse.Business.Entities.Localizations
+{
+    public record AddressNumber
+    {
+        public AddressNumber(ushort number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentOutOfRangeException(paramName: nameof(number), "O número deve ser maior do que zero");
+            }
+            Number = number;
+        }
+
+        private ushort Number { get; init; }
+
+        public ushort GetNumber()
+        {
+            return Number;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Localizations/AddressNumberTests.cs
@@ -0,0 +1,26 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using System;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Localizations
+{
+    [Collection("Business.Entities")]
+    public class AddressNumberTests
+    {
+        [Theory]
+        [InlineData(1000)]
+        [InlineData(777)]
+        public void AddressNumber_ValidAddressNumber_ShouldCreateAddressNumber(ushort number)
+        {
+            AddressNumber addressNumber = new(number);
+            Assert.Equal(number, addressNumber.GetNumber());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public void AddressNumber_InvalidAddressNumber_ShouldNotCreateAddressNumber(ushort number)
+        {
+            Assert.ThrowsAny<Exception>(() => new AddressNumber(number));
+        }
+    }
+}
