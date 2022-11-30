70abb581419d77b2712da9607618e876f6344937
Lucas Dirani <lruizd@protonmail.com>
2022-11-30 15:59:02 -0300

    Criação da classe LandmarkPreferences junto de seu teste unitário.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/Localizations/LandmarkPreferences.cs
@@ -0,0 +1,56 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using System;
+
+namespace LibHouse.Business.Entities.Residents.Preferences.Localizations
+{
+    public record LandmarkPreferences
+    {
+        public LandmarkPreferences(Address address)
+        {
+            if (address is null)
+            {
+                throw new ArgumentNullException(paramName: nameof(address), message: "O endereço de referência é obrigatório");
+            }
+            LandmarkAddress = address;
+            LandmarkAddressId = address.Id;
+        }
+
+        public Guid LandmarkAddressId { get; init; }
+        private Address LandmarkAddress { get; init; }
+
+        public string GetLandmarkAddress()
+        {
+            return LandmarkAddress.GetName();
+        }
+
+        public ushort GetLandmarkNumber()
+        {
+            return LandmarkAddress.GetNumber();
+        }
+
+        public string GetLandmarkNeighborhood()
+        {
+            return LandmarkAddress.GetNeighborhoodName();
+        }
+
+        public string GetLandmarkCity()
+        {
+            return LandmarkAddress.GetCityName();
+        }
+
+        public string GetLandmarkFederativeUnit()
+        {
+            return LandmarkAddress.GetAbbreviationOfTheFederativeUnit();
+        }
+
+        public string GetLandmarkPostalCodeNumber()
+        {
+            return LandmarkAddress.GetPostalCodeNumber();
+        }
+
+        public string GetLandmarkComplement()
+        {
+            return LandmarkAddress.GetComplement();
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/Localizations/LandmarkPreferencesTests.cs
@@ -0,0 +1,32 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using LibHouse.Business.Entities.Residents.Preferences.Localizations;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.Localizations
+{
+    [Collection("Business.Entities")]
+    public class LandmarkPreferencesTests
+    {
+        [Fact]
+        public void LandmarkPreferences_ValidLandmarkPreferences_ShouldCreateLandmarkPreferences()
+        {
+            string landmarkStreet = "Rua São Bento";
+            string landmarkComplement = "de 321 ao fim - lado ímpar";
+            ushort landmarkNumber = 321;
+            string landmarkNeighborhood = "Centro";
+            string landmarkCity = "São Paulo";
+            string landmarkFederativeUnit = "SP";
+            string landmarkPostalCodeNumber = "01011100";
+            Address landmarkAddress = new(landmarkStreet, landmarkNumber, landmarkNeighborhood, landmarkCity, landmarkFederativeUnit, landmarkPostalCodeNumber, landmarkComplement);
+            LandmarkPreferences landmarkPreferences = new(landmarkAddress);
+            Assert.Equal(landmarkStreet, landmarkPreferences.GetLandmarkAddress());
+            Assert.Equal(landmarkNumber, landmarkPreferences.GetLandmarkNumber());
+            Assert.Equal(landmarkNeighborhood, landmarkPreferences.GetLandmarkNeighborhood());
+            Assert.Equal(landmarkCity, landmarkPreferences.GetLandmarkCity());
+            Assert.Equal(landmarkFederativeUnit, landmarkPreferences.GetLandmarkFederativeUnit());
+            Assert.Equal(landmarkPostalCodeNumber, landmarkPreferences.GetLandmarkPostalCodeNumber());
+            Assert.Equal(landmarkComplement, landmarkPreferences.GetLandmarkComplement());
+            Assert.Equal(landmarkAddress.Id, landmarkPreferences.LandmarkAddressId);
+        }
+    }
+}
