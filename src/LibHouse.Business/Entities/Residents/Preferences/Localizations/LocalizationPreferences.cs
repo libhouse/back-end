b37050a4c3dacfb6b70b5c2ffbca80d2e96bbd95
Lucas Dirani <lruizd@protonmail.com>
2022-11-30 16:09:03 -0300

    Criação da classe LocalizationPreferences junto de seu teste unitário.

---
+++ src/LibHouse.Business/Entities/Residents/Preferences/Localizations/LocalizationPreferences.cs
@@ -0,0 +1,19 @@
+﻿using LibHouse.Business.Entities.Localizations;
+
+namespace LibHouse.Business.Entities.Residents.Preferences.Localizations
+{
+    public class LocalizationPreferences
+    {
+        public LandmarkPreferences LandmarkPreferences { get; private set; }
+
+        public void AddLandmarkPreferences(Address landmarkAddress)
+        {
+            LandmarkPreferences = new(landmarkAddress);
+        }
+
+        public bool HaveLandmarkPreferences()
+        {
+            return LandmarkPreferences is not null;
+        }
+    }
+}
---
+++ tests/LibHouse.UnitTests/Suite/Business/Entities/Residents/Preferences/Localizations/LocalizationPreferencesTests.cs
@@ -0,0 +1,26 @@
+﻿using LibHouse.Business.Entities.Localizations;
+using LibHouse.Business.Entities.Residents.Preferences.Localizations;
+using Xunit;
+
+namespace LibHouse.UnitTests.Suite.Business.Entities.Residents.Preferences.Localizations
+{
+    [Collection("Business.Entities")]
+    public class LocalizationPreferencesTests
+    {
+        [Fact]
+        public void AddLandmarkPreferences_ValidLandmarkPreferences_ShouldAddLandmarkPreferences()
+        {
+            LocalizationPreferences localizationPreferences = new();
+            string landmarkStreet = "Rua São Bento";
+            string landmarkComplement = "de 321 ao fim - lado ímpar";
+            ushort landmarkNumber = 321;
+            string landmarkNeighborhood = "Centro";
+            string landmarkCity = "São Paulo";
+            string landmarkFederativeUnit = "SP";
+            string landmarkPostalCodeNumber = "01011100";
+            Address landmarkAddress = new(landmarkStreet, landmarkNumber, landmarkNeighborhood, landmarkCity, landmarkFederativeUnit, landmarkPostalCodeNumber, landmarkComplement);
+            localizationPreferences.AddLandmarkPreferences(landmarkAddress);
+            Assert.True(localizationPreferences.HaveLandmarkPreferences());
+        }
+    }
+}
