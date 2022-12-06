c9ea4dee09b7c9307dd60fb8b2a1714f3fc83331
Lucas Dirani <lruizd@protonmail.com>
2022-12-06 17:00:08 -0300

    Criação de índice para a tabela Address.

---
+++ src/LibHouse.Data/Migrations/20221206195348_Index_Address_PostalCodeAndNumber.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibHouse.Data.Migrations
+{
+    public partial class Index_Address_PostalCodeAndNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE NONCLUSTERED INDEX [idx_address_postalcode_number] ON [Business].[Address]([PostalCode],[Number] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX [idx_address_postalcode_number] ON [Business].[Address]");
+        }
+    }
+}
