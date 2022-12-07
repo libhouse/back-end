94c4fe5613d103cf837a61a97f92f9e969101334
Lucas Dirani <lruizd@protonmail.com>
2022-12-07 16:06:32 -0300

    Inclusão de Data Annotations para a classe ResidentGeneralPreferencesRegistrationViewModel.

--- src/LibHouse.Infrastructure.Controllers/ViewModels/Residents/ResidentGeneralPreferencesRegistrationViewModel.cs
+++ src/LibHouse.Infrastructure.Controllers/ViewModels/Residents/ResidentGeneralPreferencesRegistrationViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibHouse.Infrastructure.Controllers.ViewModels.Residents
 {
@@ -10,38 +11,47 @@ namespace LibHouse.Infrastructure.Controllers.ViewModels.Residents
         /// <summary>
         /// A identificação do morador que terá as suas preferências cadastradas
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public Guid ResidentId { get; init; }
         /// <summary>
         /// Indica se o morador deseja morar em uma casa que aceite animais
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public bool WantSpaceForAnimals { get; init; }
         /// <summary>
         /// Indica se o morador deseja morar em uma casa que aceite crianças
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public bool AcceptChildren { get; init; }
         /// <summary>
         /// Indica se o morador deseja morar em uma casa que aceite festas
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public bool WantsToParty { get; init; }
         /// <summary>
         /// Indica se o morador deseja morar em uma casa que aceite fumantes
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public bool AcceptSmokers { get; init; }
         /// <summary>
         /// Indica se o morador deseja morar em uma casa que aceite somente homens
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public bool AcceptsOnlyMenAsRoommates { get; init; }
         /// <summary>
         /// Indica se o morador deseja morar em uma casa que aceite somente mulheres
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public bool AcceptsOnlyWomenAsRoommates { get; init; }
         /// <summary>
         /// Indica a quantidade mínima de colegas de quarto que o morador deseja ter
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public int MinimumNumberOfRoommatesDesired { get; init; }
         /// <summary>
         /// Indica a quantidade máxima de colegas de quarto que o morador deseja ter
         /// </summary>
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public int MaximumNumberOfRoommatesDesired { get; init; }
     }
 }
